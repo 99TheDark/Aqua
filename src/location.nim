2632144350338f2d9201e7f89de73c75c2534de7
99TheDark <99TheDark@gmail.com>
2024-04-06 22:24:34 -0700

    Added shift and next methods to location

--- src/location.nim
+++ src/location.nim
@@ -3,4 +3,10 @@ type Location* = ref object of RootObj
   row*: int
   col*: int
 
+proc shift*(self: Location, count: int) =
+  self.idx += count
+  self.col += count
+
+proc next*(self: Location) = self.shift(1)
+
 proc emptyLoc*(): Location = Location(idx: 0, row: 0, col: 0)
